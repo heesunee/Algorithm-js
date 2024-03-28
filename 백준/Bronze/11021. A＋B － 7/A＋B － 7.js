7f24382dd50fdf0ad42248a3316f9128524657f3
heesun <91944542+heesun729@users.noreply.github.com>
2024-03-28 16:27:20 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 9512 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+let t = +input[0];
+
+for (let i = 1; i <= t; i++) {
+    let num = input[i].split(' ').map((item) => +item);
+    console.log(`Case #${i}: ${num[0] + num[1]}`);
+}
