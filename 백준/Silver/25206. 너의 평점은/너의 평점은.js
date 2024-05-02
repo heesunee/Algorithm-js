c3a8d29fcc71f2d69c255ffeb45e1359aa1993d6
heesun <91944542+heesun729@users.noreply.github.com>
2024-05-02 18:52:04 +0900

    [Silver V] Title: 너의 평점은, Time: 132 ms, Memory: 9612 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs
+    .readFileSync(filePath)
+    .toString()
+    .trim()
+    .split('\n')
+    .map((item) => item.split(' '));
+
+let sum = 0;
+let total = 0;
+
+const table = {
+    'A+': 4.5,
+    A0: 4.0,
+    'B+': 3.5,
+    B0: 3.0,
+    'C+': 2.5,
+    C0: 2.0,
+    'D+': 1.5,
+    D0: 1.0,
+    F: 0,
+};
+for (let [sub, score, grade] of input) {
+    if (grade === 'P') continue;
+
+    sum += Number(score) * table[grade];
+    total += Number(score);
+}
+
+console.log(sum / total);
