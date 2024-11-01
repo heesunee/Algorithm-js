95206f6d4c2240f0fce67082ad333f548e007b74
Heesun Park <91944542+heesunee@users.noreply.github.com>
2024-11-01 09:58:50 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 92 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.js
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.js
@@ -1,33 +1,25 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const [N, pattern, ...input] = fs
-  .readFileSync(filePath)
+// 5번 문제: Korea.js
+
+const solution = (N, pattern, array) => {
+  const fileName = pattern.split("*");
+
+  return array
+    .map((el) => {
+      if (
+        el.startsWith(fileName[0]) &&
+        el.slice(fileName[0].length).endsWith(fileName[1])
+      ) {
+        return "DA";
+      }
+      return "NE";
+    })
+    .join("\n");
+};
+
+const [N, pattern, ...array] = require("fs")
+  .readFileSync("/dev/stdin")
   .toString()
   .trim()
-  .split('\n');
+  .split("\n");
 
-function solution() {
-  const findPattern = pattern.trim();
-  const starIndex = findPattern.indexOf('*'); // 별표의 인덱스 찾기
-
-  // 별표를 기준으로 앞부분과 뒷부분 분리
-  const startPattern = findPattern.slice(0, starIndex);
-  const endPattern = findPattern.slice(starIndex + 1);
-
-  input.forEach((word) => {
-    // 각 파일 이름에 대해 시작 패턴과 끝 패턴을 확인
-    const startsWithPattern = word.startsWith(startPattern);
-    const endsWithPattern = word.endsWith(endPattern);
-    
-    // 파일 이름의 길이를 확인하여 두 패턴이 맞는지 판단
-    if (startsWithPattern && endsWithPattern && 
-        word.length >= (startPattern.length + endPattern.length)) {
-      console.log('DA');
-    } else {
-      console.log('NE');
-    }
-  });
-}
-
-solution();
-
+console.log(solution(Number(N), pattern, array));
