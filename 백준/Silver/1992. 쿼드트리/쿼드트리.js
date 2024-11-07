48b8dd73351d6e4feeb7eb2848a9fe0c5540c714
Heesun Park <91944542+heesunee@users.noreply.github.com>
2024-11-08 00:25:03 +0900

    [Silver I] Title: 쿼드트리, Time: 152 ms, Memory: 10268 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.js
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.js
@@ -1,30 +1,35 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-const n = +input[0];
-const screen = input.slice(1).map(v => v.split("").map(vv => +vv));
+// 5번 문제: 쿼드트리(quadTree.js)
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+const [N, ...arr] = input;
+const area = arr.map((item) => item.split("").map(Number));
 
-const genQuadTree = n => {
-    const quadTree = [];
-    const recursion = (n, x, y) => {
-        let total = 0;
-        for(let i=0; i<n; i++) {
-            for(let j=0; j<n; j++) {
-                total += screen[y+j][x+i];
-            }
-        }
-        if (total === 0) quadTree.push("0");
-        else if (total === n*n) quadTree.push("1");
-        else {
-            n /= 2;
-            quadTree.push("(");
-            recursion(n, x, y);
-            recursion(n, x+n, y);
-            recursion(n, x, y+n);
-            recursion(n, x+n, y+n);
-            quadTree.push(")");
-        }
+const quadTree = [];
+
+function recursion(n, x, y) {
+  let total = 0;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      total += area[y + j][x + i];
     }
-    recursion(n, 0, 0);
-    console.log(quadTree.join(""));
-};
+  }
+  if (total === 0) quadTree.push(0);
+  else if (total === n * n) quadTree.push(1);
+  else {
+    n /= 2;
+    quadTree.push("(");
+    recursion(n, x, y);
+    recursion(n, x + n, y);
+    recursion(n, x, y + n);
+    recursion(n, x + n, y + n);
+    quadTree.push(")");
+  }
+}
 
-genQuadTree(n);
+recursion(N, 0, 0);
+console.log(quadTree.join(""));
