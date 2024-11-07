8c6fb1568046aafd7883c29498d11d3d4f9ed17f
Heesun Park <91944542+heesunee@users.noreply.github.com>
2024-11-08 00:33:36 +0900

    [Silver V] Title: 사과 담기 게임, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.js
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.js
@@ -1,29 +1,35 @@
-// 6번 문제: 사과 담기 게임(appleGame.js)
+//1: N과 M
+//2: 떨어지는 사과의 개수 (1 ≤ J ≤ 20)
+//3: J개 줄에는 사과가 떨어지는 위치가 순서대로 주어진다.
+//출력: 모든 사과를 담기 위해 바구니가 이동해야 하는 거리의 최솟값 출력
+
 const fs = require("fs");
-const input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const [N, M] = input.shift().split(" ").map(Number);
-input.shift();
-const apples = input.map(Number);
+const [n, m] = input[0].split(" ").map(Number);
+const num = parseInt(input[1]);
 
-let left = 1;
-let right = M;
-let move = 0;
+let start = 1;
+let end = start + m;
+let answer = 0;
 
-for (let i = 0; i < apples.length; i++) {
-  const apple = apples[i];
-  if (apple >= left && apple <= right) continue;
-  else if (apple < left) {
-    const dist = left - apple;
-    left = apple;
-    right = right - dist;
-    move += dist;
-  } else {
-    apple > right;
-    const dist = apple - right;
-    right = apple;
-    left = left + dist;
-    move += dist;
-  }
+for (let i = 2; i < 2 + num; i++) {
+    const a = parseInt(input[i]);
+
+    if (a >= start && a < end) {
+        // 바구니가 이미 사과를 포함하는 경우 이동할 필요 없음
+        continue;
+    } else if (a < start) {
+        // 사과가 바구니의 왼쪽에 있는 경우
+        answer += start - a;
+        start = a;
+        end = start + m;
+    } else {
+        // 사과가 바구니의 오른쪽에 있는 경우
+        answer += a + 1 - end;
+        end = a + 1;
+        start = end - m;
+    }
 }
-console.log(move);
+
+console.log(answer);
