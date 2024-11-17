609920e21820fbcc1a8d86eb1ef5cdad336e47e6
Heesun Park <91944542+heesunee@users.noreply.github.com>
2024-11-17 15:06:58 +0900

    [Gold IV] Title: 치즈, Time: 188 ms, Memory: 13420 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.js
+++ 백준/Gold/2636. 치즈/치즈.js
@@ -1,97 +1,63 @@
-class Queue {
-  constructor() {
-    this.dat = [];
-    this.head = 0;
-    this.tail = 0;
-  }
+// 7번 문제: 치즈(cheese.js)
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
-  push(item) {
-    this.dat[this.tail++] = item;
-  }
+let [N, M] = input[0].split(" ").map((v) => +v);
 
-  pop() {
-    this.head++;
-  }
+let graph = input.slice(1).map((v) => v.split(" "));
 
-  front() {
-    return this.dat[this.head];
-  }
+const dx = [0, 1, 0, -1];
+const dy = [1, 0, -1, 0];
 
-  rear() {
-    return this.dat[this.tail - 1];
-  }
-
-  isEmpty() {
-    return this.head === this.tail;
-  }
-}
-
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = require('fs')
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split('\n');
-
-const [N, M] = input[0].split(' ').map(Number);
-const board = Array.from({ length: N }, () => Array(M));
-for (let i = 1; i <= N; i++) {
-  const temp = input[i].split(' ').map(Number);
-  for (let j = 0; j < M; j++) {
-    board[i - 1][j] = temp[j];
-  }
-}
-const dir = [
-  [-1, 0],
-  [1, 0],
-  [0, -1],
-  [0, 1],
-];
 let time = 0;
-let cnt = 0;
+let answer = 0;
+let queue = [];
 
-function bfs() {
-  const queue = new Queue();
-  const visited = Array.from({ length: N }, () => Array(M).fill(false));
-  queue.push([0, 0]);
-  visited[0][0] = true;
-  let nodes = [];
-
-  while (!queue.isEmpty()) {
-    const [x, y] = queue.front();
-    queue.pop();
-
-    for (let k = 0; k < 4; k++) {
-      const nx = x + dir[k][0];
-      const ny = y + dir[k][1];
-
-      if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-      if (visited[nx][ny]) continue;
-      if (board[nx][ny] === 1) {
-        nodes = [...nodes, [nx, ny]]; // push 메서드 대신
-        visited[nx][ny] = true; // 배열에 중복되는 좌표가 들어가는 것을 방지
-        continue;
+function DFS(startNode, visited, queue) {
+  let [x, y] = startNode;
+  if (graph[x][y] === "1") return;
+  for (let i = 0; i < 4; i++) {
+    const nx = x + dx[i];
+    const ny = y + dy[i];
+    if (nx >= 0 && ny >= 0 && nx < N && ny < M && visited[nx][ny] === -1) {
+      if (graph[nx][ny] === "1") {
+        visited[nx][ny] = 1;
+        queue.push([nx, ny]);
+      } else {
+        visited[nx][ny] = 0;
       }
-      queue.push([nx, ny]);
-      visited[nx][ny] = true;
+      visited[x][y] = 0;
+      DFS([nx, ny], visited, queue);
     }
   }
+}
 
-  for (const [x, y] of nodes) {
-    board[x][y] = 0;
+function getCount(graph) {
+  let sum = 0;
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+      if (graph[i][j] === "1") sum++;
+    }
   }
+  return sum;
+}
 
-  if (!nodes.length) return true;
-
-  cnt = nodes.length;
-  return false;
+function melt() {
+  let q = queue.slice();
+  queue = [];
+  while (q.length) {
+    let [x, y] = q.shift();
+    graph[x][y] = "0";
+  }
 }
 
 while (true) {
-  if (bfs()) break;
-
-  // 제거되는 치즈가 존재할 경우에만 시간을 증가시킨다.
-  time += 1;
+  let visited = Array.from(Array(N), () => Array(M).fill(-1));
+  DFS([0, 0], visited, queue);
+  if (!queue.length) break;
+  answer = getCount(graph);
+  melt();
+  time++;
 }
 
-console.log(time, cnt);
+console.log(time + "\n" + answer);
