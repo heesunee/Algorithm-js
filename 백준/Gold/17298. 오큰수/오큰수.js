97144b9bcf71bf94aa0d2d3c9c3f67cff932da24
Heesun Park <91944542+heesunee@users.noreply.github.com>
2024-11-17 15:07:31 +0900

    [Gold IV] Title: 오큰수, Time: 740 ms, Memory: 184284 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.js
+++ 백준/Gold/17298. 오큰수/오큰수.js
@@ -1,26 +1,22 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+// 10번 문제: 오큰수(NGE.js)
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
-const input = [];
+const N = Number(input[0]);
+const A = input[1].split(" ").map(Number);
+const stack = [];
 
-readline.on('line', function(line) {
-    input.push(line);  
-}).on('close', function() {
-    const N = Number(input[0]);
-    const A = input[1].split(' ').map(Number);
-    const result = Array(N).fill(-1);  // 결과 배열을 -1로 초기화
-    const stack = [];
+// 오큰수 찾기 알고리즘
+for (let i = 0; i < N; i++) {
+  while (stack.length && Number(A[i]) > Number(A[stack[stack.length - 1]])) {
+    A[stack.pop()] = A[i];
+  }
+  stack.push(i);
+}
 
-    for (let i = 0; i < N; i++) {
-        // 현재 값이 스택의 맨 위에 있는 인덱스 값보다 클 때 오큰수로 
-        while (stack.length > 0 && A[stack[stack.length - 1]] < A[i]) {
-            result[stack.pop()] = A[i];
-        }
-        stack.push(i); // 인덱스를 스택에 추가
-    }
+// 스택에 남아있는 원소 처리
+while (stack.length) {
+  A[stack.pop()] = -1;
+}
 
-    console.log(result.join(' '));
-    process.exit();
-});
+console.log(A.join(" ").trim());
