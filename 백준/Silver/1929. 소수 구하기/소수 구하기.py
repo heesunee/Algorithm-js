b70fb766b9e7da126bb2a184c718faac3f3fac08
Heesunee <91944542+heesunee@users.noreply.github.com>
2025-04-09 18:13:28 +0900

    [Silver III] Title: 소수 구하기, Time: 316 ms, Memory: 40084 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,10 +1,19 @@
-m, n = map(int, input().split(' '))
+import sys
+input = sys.stdin.readline
 
-for i in range (m, n+1):
-    if i<2:
-        continue
-    for j in range(2, int(i**0.5)+1):
-        if i%j ==0:
-            break
-    else:
+m, n = map(int, input().split())
+
+# 소수 여부를 저장할 리스트
+is_prime = [True] * (n+1)
+is_prime[0] = False
+is_prime[1] = False
+
+# 2부터 루트n까지 반복하며 배수 지우기
+for i in range (2, int(n**0.5) +1):
+    if is_prime[i]:
+        for j in range(i*i, n+1, i):
+            is_prime[j] = False
+
+for i in range(m, n+1):
+    if is_prime[i]:
         print(i)
