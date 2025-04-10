5169eacf969fae78526eb1edfdf478caaf690ef9
Heesunee <91944542+heesunee@users.noreply.github.com>
2025-04-10 20:08:11 +0900

    [Silver II] Title: 베르트랑 공준, Time: 188 ms, Memory: 12356 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.js
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.js
@@ -5,25 +5,26 @@ const input = require('fs')
   .split('\n')
   .map(Number);
 
-function isPrime(n) {
-  const array = new Array(2 * n + 1).fill(true);
-  array[0] = array[1] = false;
+const maxNum = 2 * Math.max(...input);
 
-  for (let i = 2; i * i <= 2 * n; i++) {
-    if (array[i]) {
-      for (let j = i * i; j <= 2 * n; j += i) {
-        array[j] = false;
-      }
+const array = new Array(maxNum + 1).fill(true);
+array[0] = array[1] = false;
+
+for (let i = 2; i * i <= maxNum; i++) {
+  if (array[i]) {
+    for (let j = i * i; j <= maxNum; j += i) {
+      array[j] = false;
     }
   }
-  let result = 0;
-  for (let i = n + 1; i <= 2 * n; i++) {
-    if (array[i]) result++;
-  }
-
-  return result;
 }
 
 for (let i = 0; i < input.length - 1; i++) {
-  console.log(isPrime(input[i]));
+  const n = input[i];
+  const max = n * 2;
+  let count = 0;
+  for (let i = n + 1; i <= max; i++) {
+    if (array[i]) count++;
+  }
+
+  console.log(count);
 }
