0d6d71800c227d655f1b765dbabbc50dc801eed6
Heesunee <91944542+heesunee@users.noreply.github.com>
2025-04-11 13:06:20 +0900

    [Bronze V] Title: A+B - 3, Time: 100 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -1,10 +1,11 @@
- const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n');
+const [T, ...input] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split('\n');
 
-let t = +input[0];
+const array = input.map((cur) => cur.trim().split(' ').map(Number));
 
-for (let i = 1; i <= t; i++) {
-    let num = input[i].split(' ').map((item) => +item);
-    console.log(num[0] + num[1]);
+for (let i = 0; i < T; i++) {
+  console.log(array[i][0] + array[i][1]);
 }
