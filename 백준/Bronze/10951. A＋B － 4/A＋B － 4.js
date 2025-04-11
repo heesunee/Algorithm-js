aa75847534600f304cbe82e96662d55ebc72e748
Heesunee <91944542+heesunee@users.noreply.github.com>
2025-04-11 13:10:42 +0900

    [Bronze V] Title: A+B - 4, Time: 100 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -1,10 +1,10 @@
-const input = require("fs")
-    .readFileSync("/dev/stdin")
-    .toString()
-    .trim()
-    .split("\n");
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split('\n');
 
-for (i = 0; i <= input.length - 1; i++) {
-    let [a, b] = input[i].split(" ").map((value) => +value);
-    console.log(a + b);
+for (let i = 0; i < input.length; i++) {
+  const [A, B] = input[i].split(' ').map(Number);
+  console.log(A + B);
 }
