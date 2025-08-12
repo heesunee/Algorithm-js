6800dbb16dcc0f89444f74399d1235e1d365d4e8
Heesunee <91944542+heesunee@users.noreply.github.com>
2025-08-12 17:39:14 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.js
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.js
@@ -1,43 +1,29 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filePath).toString().trim().split('').sort();
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split('');
 
-function solution() {
-  const charCount = {};
-
-  // 각 문자의 개수 세기
-  input.forEach((char) => {
-    charCount[char] = (charCount[char] || 0) + 1;
-  });
-
-  let oddCount = 0;
-  let oddChar = '';
-
-  // 홀수 개수의 문자를 확인
-  for (const char in charCount) {
-    if (charCount[char] % 2 !== 0) {
-      oddCount++;
-      oddChar = char; // 홀수 문자를 저장
-    }
-  }
-
-  // 홀수 개수의 문자가 1개 이상이면 팰린드롬 불가능
-  if (oddCount > 1) {
-    console.log("I'm Sorry Hansoo");
-    return;
-  }
-
-  let halfPalindrome = '';
-
-  // 팰린드롬의 반을 만들기
-  for (const char in charCount) {
-    halfPalindrome += char.repeat(charCount[char] / 2);
-  }
-
-  // 전체 팰린드롬 문자열 생성
-  const palindrome =
-    halfPalindrome + oddChar + halfPalindrome.split('').reverse().join('');
-  console.log(palindrome);
+const arr = Array(26).fill(0);
+for (const ch of input) {
+  arr[ch.charCodeAt(0) - 65]++;
 }
 
-solution();
+let oddIdx = -1, oddCnt = 0;
+for (let i = 0; i < 26; i++) {
+  if (arr[i] % 2 === 1) { oddCnt++; oddIdx = i; }
+}
+if (oddCnt > 1) {
+  console.log("I'm Sorry Hansoo");
+  process.exit(0);
+}
+
+let left = '';
+for (let i = 0; i < 26; i++) {
+  left += String.fromCharCode(65 + i).repeat(Math.floor(arr[i] / 2)); // ← 여기!
+}
+
+const mid = oddIdx !== -1 ? String.fromCharCode(65 + oddIdx) : '';
+const right = left.split('').reverse().join('');
+
+console.log(left + mid + right);
