6cf3d34611f257a2a45fea1ce4e59da51c5742f6
Heesunee <91944542+heesunee@users.noreply.github.com>
2025-08-20 13:37:22 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 9428 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -4,36 +4,27 @@ const input = require('fs')
   .trim()
   .split('\n');
 
-const COM = +input[0];
-const V = +input[1];
+const N = Number(input[0]);
+const M = Number(input[1]);
 
-const graph = Array.from({ length: COM + 1 }, () => []);
+const graph = Array.from({ length: N + 1 }, () => []);
 
-for (let i = 2; i < 2 + V; i++) {
-  const [n1, n2] = input[i].split(' ').map(Number);
-  graph[n1].push(n2);
-  graph[n2].push(n1);
+for (let i = 2; i < input.length; i++) {
+  let [x, y] = input[i].split(' ').map(Number);
+  graph[x].push(y);
+  graph[y].push(x);
 }
 
-const bfs = (start) => {
-  const queue = [start];
-  const visited = Array(COM + 1).fill(false);
-  let count = 0;
-  visited[start] = true;
+let cnt = 0;
+let visited = new Array(N + 1).fill(false);
 
-  while (queue.length > 0) {
-    const node = queue.shift();
-    const nodeVisit = [...graph[node]].sort((a, b) => a - b);
-
-    for (const next of nodeVisit) {
-      if (!visited[next]) {
-        visited[next] = true;
-        queue.push(next);
-        count++;
-      }
-    }
+const dfs = (x) => {
+  visited[x] = true;
+  cnt++;
+  for (y of graph[x]) {
+    if (!visited[y]) dfs(y);
   }
-  return count;
 };
 
-console.log(bfs(1));
+dfs(1);
+console.log(cnt - 1);
